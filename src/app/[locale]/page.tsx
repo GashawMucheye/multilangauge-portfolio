3b8a5302b8a9c1ad39fc530685bcf761a6def189
gashaw mucheye <gashaw2533@gmail.com>
2025-08-20 11:54:50 +0200

    install next-intl make the app multi-lang

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { Link } from '@/src/i18n/navigation';
+import { useTranslations } from 'next-intl';
+
+export default function HomePage() {
+  const t = useTranslations('HomePage');
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <Link href='/about'>{t('about')}</Link>
+      <Button>button</Button>
+    </div>
+  );
+}
